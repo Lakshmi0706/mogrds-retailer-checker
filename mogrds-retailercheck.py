c58fe49e73971a8ea429ff6cc4368443e0fcccae
Lakshmi0706 <lakshmi07062002@gmail.com>
2025-09-14 12:07:42 +0530

    mogrds-retailercheck.py

--- mogrds-retailercheck.py
+++ mogrds-retailercheck.py
@@ -6,8 +6,28 @@ from io import BytesIO
 
 st.title("Retailer Search Validator")
 
+# Upload file
 uploaded_file = st.file_uploader("Upload a file containing descriptions", type=["csv", "xlsx"])
+
+# Input SerpAPI key
 api_key = st.text_input("Enter your SerpAPI key")
+
+# List of known non-retail domains to ignore
+non_retail_domains = {
+    "instagram.com", "facebook.com", "linkedin.com", "youtube.com", "twitter.com",
+    "pinterest.com", "tiktok.com", "reddit.com", "wikipedia.org", "yelp.com"
+}
+
+def extract_retailer_name(result):
+    title = result.get("title", "")
+    snippet = result.get("snippet", "")
+    for text in [title, snippet]:
+        words = text.split()
+        for word in words:
+            word_clean = word.strip(",.()").lower()
+            if word_clean not in non_retail_domains and word_clean.isalpha():
+                return word_clean.title()
+    return None
 
 if uploaded_file and api_key:
     if uploaded_file.name.endswith('.csv'):
@@ -31,18 +51,21 @@ if uploaded_file and api_key:
             response = requests.get("https://serpapi.com/search", params=params)
             data = response.json()
 
-            domains = set()
+            retailers = set()
             if "organic_results" in data:
                 for result in data["organic_results"]:
-                    if "link" in result:
-                        domain = urlparse(result["link"]).netloc
-                        domains.add(domain)
+                    link = result.get("link", "")
+                    domain = urlparse(link).netloc.replace("www.", "")
+                    if domain not in non_retail_domains:
+                        retailer_name = extract_retailer_name(result)
+                        if retailer_name:
+                            retailers.add(retailer_name)
 
-            if len(domains) == 1:
-                df.at[i, 'retailer'] = list(domains)[0]
+            if len(retailers) == 1:
+                df.at[i, 'retailer'] = list(retailers)[0]
                 df.at[i, 'status'] = 'Yes'
             else:
-                df.at[i, 'retailer'] = ', '.join(domains)
+                df.at[i, 'retailer'] = ', '.join(retailers)
                 df.at[i, 'status'] = 'No'
 
         st.write("Updated Data:")
